8ddd842aec2357ba92577b4b47501e02b5e70676
Giorgi Baghashvili <giorgibaghashvili@gmail.com>
2018-03-16 23:35:59 +0400

    admin 3

--- database/migrations/2018_03_16_180846_entrust_setup_tables.php
+++ database/migrations/2018_03_16_180846_entrust_setup_tables.php
@@ -17,7 +17,7 @@ class EntrustSetupTables extends Migration
             $table->string('name')->unique();
             $table->string('display_name')->nullable();
             $table->string('description')->nullable();
-            $table->timestamps();
+            $table->timestamps(); 
         });
 
         // Create table for associating roles to users (Many-to-Many)
